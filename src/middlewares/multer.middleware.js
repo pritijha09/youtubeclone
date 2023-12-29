7f4ca5d23ba75fb7cb04283c6ad2ca2d67417ea5
pritijha09 <jhapriti09@gmail.com>
2023-12-29 16:57:27 +0530

    implement file upload utilty

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from 'multer'; 
+
+const storage = multer.diskStorage({
+    destination: function(req, res, cb) {
+        cb(null, './public/temp')
+    },
+    filename: function (req, file, cb) {
+       // const uniqueSuffix = Date.now() + '-'+Math.round(Math.random()*IE9)
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({storage: storage})
