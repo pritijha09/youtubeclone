fc5480668383f56a70c9c9e5d1a108c1deeb7070
pritijha09 <jhapriti09@gmail.com>
2024-01-02 17:43:10 +0530

    implement user register

--- src/app.js
+++ src/app.js
@@ -12,5 +12,11 @@ app.use(express.urlencoded({extended: true, limit: '10kb'}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes
+import  userRouter from './routes/user.routes.js'
 
+//routes declarations
+app.use("/api/v1/users", userRouter);
+
+//http://localhost:8000/api/v1/users/register
 export default app; 
